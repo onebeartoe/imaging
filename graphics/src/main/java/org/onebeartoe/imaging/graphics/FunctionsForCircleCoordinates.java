2605f11beea9af77c19ed9a8fefd88965e09a6bb
onebeartoe <onebeartoe@gmail.com>
2017-04-13 08:35:37 -0500

    This is part of the move refactor.

--- graphics/src/main/java/org/onebeartoe/imaging/graphics/FunctionsForCircleCoordinates.java
+++ graphics/src/main/java/org/onebeartoe/imaging/graphics/FunctionsForCircleCoordinates.java
@@ -14,10 +14,8 @@ public class FunctionsForCircleCoordinates
         double x = Math.cos( (14*3.14*i)/12000.0 ) * ( 1 - 3/4.0 * Math.pow( Math.cos( (32 * 3.14 * i) / 12000.0 ) , 2 ) );
                     
         double y = Math.sin( (14*3.14*i)/12000.0 ) * ( 1 - 3/4.0 * Math.pow( Math.cos( (32 * 3.14 * i) / 12000.0 ) , 2 ) );
-
                     
         double radius = 1/200.0 + 1/10.0 * Math.pow(  Math.sin( (56*3.14*i)/12000.0 )  ,6);
-
                 
         Circle c = new Circle(x, y, radius);
         
