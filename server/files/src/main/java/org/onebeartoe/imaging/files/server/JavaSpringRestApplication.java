11e34e57a78c83decc83c93999f993a8e5885469
Roberto Marquez <onebeartoe@gmail.com>
2021-01-15 19:42:04 -0700

    temporarily change the image path and whitespace

--- server/files/src/main/java/org/onebeartoe/imaging/files/server/JavaSpringRestApplication.java
+++ server/files/src/main/java/org/onebeartoe/imaging/files/server/JavaSpringRestApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages={"org.onebeartoe.imaging"})
 public class JavaSpringRestApplication extends SpringBootServletInitializer
 {
     @Override 
